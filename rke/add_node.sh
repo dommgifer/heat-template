683c4e132a0a63d055b49b7f3d767c6eaf5d30fa
dommgifer <dommgifer@gmail.com>
2019-06-17 16:50:03 +0800

    update

--- rke/add_node.sh
+++ rke/add_node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -v
-echo $kube_minion_ip > /opt/ip.txt
+echo $KUBE_MINION_IP > /opt/ip.txt
 #wget https://raw.githubusercontent.com/dommgifer/heat-template/master/rke/ssh.sh
 #mv ssh.sh /opt/ssh.sh
 #for ((i=1; i<=$count; i++))
